74694ba79e39cf6c37529e51bd5bbb313dff9cfa
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2024-04-29 17:53:47 +0300

    fixed

--- frontend/src/components/ui/signInPage/components/form/form.tsx
+++ frontend/src/components/ui/signInPage/components/form/form.tsx
@@ -4,17 +4,23 @@ import { checkUserData } from "../../../../../api/authApi/authApi";
 import { setDataToLocalStorage } from "../../../../../storage/localStorage/localStorage";
 import { ButtonComponent } from "../../../../shared/button/button";
 import { FormContainer, Title } from "./styledForm";
+import { useNavigate } from "react-router-dom";
 
 export const Form: FC = () => {
     const [emailValue, setEmailValue] = useState<string>("");
     const [passwordValue, setPasswordValue] = useState<string>("");
+    const navigate = useNavigate();
 
-    const logIn = async () => {
+    const logIn = async (event: any) => {
+        event.preventDefault();
+        
         try {
             const token = await checkUserData({ email: emailValue, password: passwordValue });
 
+            console.log(token)
             if (token) {
                 setDataToLocalStorage("token", token);
+                navigate('/profile');
             } else {
                 console.log("wrong data");
             }
