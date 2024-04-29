78586463e23b6588aac6cd556e6f0c8b8ee1af15
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2024-04-29 17:28:57 +0300

    added listener on icons

--- frontend/src/components/ui/expensesPage/components/categorySelectionModal/categorySelectionModal.tsx
+++ frontend/src/components/ui/expensesPage/components/categorySelectionModal/categorySelectionModal.tsx
@@ -1,10 +1,19 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { v4 as uuidV4 } from "uuid";
 import { ICONS_COLLECTION } from "../../../../../consts/images";
 import { ButtonComponent } from ".././../../../shared/button/button";
 import { BtnInner, Container, Input, InputInner, Item, Label, List, Wrapper } from "./styledCategorySelectionModal";
 
 export const CategorySelectionModal: FC = () => {
+    const [selectedIcon, setSelectedIcon] = useState();
+
+    const selectCategoryIcon = (event: any) => {
+        console.log(event.currentTarget)
+        setSelectedIcon(event.currentTarget.children[0].src)
+        event.currentTarget.parentNode.style.border = "1px solid #cacaca";
+
+        console.log(selectedIcon)
+    }
     return (
         <Container>
             <Wrapper>
@@ -15,7 +24,9 @@ export const CategorySelectionModal: FC = () => {
                 <List>
                     {ICONS_COLLECTION.slice(0, 9).map((icon) => (
                         <Item key={uuidV4()}>
-                            <img src={icon.default} alt="" />
+                            <button onClick={selectCategoryIcon} type="button">
+                                <img src={icon.default} alt="" />
+                            </button>
                         </Item>
                     ))}
                 </List>
