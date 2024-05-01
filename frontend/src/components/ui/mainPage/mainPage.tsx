3236cc8a16d8da81876b5b8862147315a8d993a0
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2024-05-01 19:48:22 +0300

    added request for changing data

--- frontend/src/components/ui/mainPage/mainPage.tsx
+++ frontend/src/components/ui/mainPage/mainPage.tsx
@@ -1,8 +1,12 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Application, FeaturesSection, Footer, Header, HelpCenterPage, HeroSection, RangSection } from ".";
+import { upd } from "../../../api/authApi/authApi";
 
 
 export const MainPage: FC = () => {
+    useEffect(() => {
+        upd()
+    })
     return (
         <>
             <Header />
