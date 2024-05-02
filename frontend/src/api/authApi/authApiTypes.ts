f63ed73a8fe5059b3a230fe7601f85b6fba21fc5
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2024-05-02 15:43:07 +0300

    added edit modal and configured changing user name

--- frontend/src/api/authApi/authApiTypes.ts
+++ frontend/src/api/authApi/authApiTypes.ts
@@ -9,3 +9,4 @@ export type SetUserDataType = (data: UserDataType) => Promise<string>;
 export type CheckUserDataType = (data: UserDataType) => Promise<string | boolean>;
 export type CheckUserDataByEmailType = (data: UserDataType) => Promise<string>;
 export type GetUserDataType = (data: string) => Promise<UserDataType>;
+export type UpdateUserDataType = (token: string, changedData: UserDataType ) => Promise<string>;
--- frontend/src/components/ui/profileChaningPages/changeNamePage/components/item/item.tsx
+++ frontend/src/components/ui/profileChaningPages/changeNamePage/components/item/item.tsx
@@ -1,12 +1,16 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
-import { useLocation } from "react-router-dom";
+import { EditUserDataModal } from "../../../../../shared/editUserDataModal/editUserDataModal";
+import { DarkBackground } from "../../../../../shared/darkBackground/darkBackground";
 import { Container, Wrapper, UserName } from "./styledItem";
 
 export const Item: FC = () => {
+    const [isModalActive, setIsModalActive] = useState<boolean>(false);
     const location = useLocation();
-    
+    const [userName, setUserName] = useState<string>(location.state);
+
     return (
         <Container>
             <Wrapper>
@@ -17,12 +21,20 @@ export const Item: FC = () => {
                         </h5>
                     </div>
                     <UserName>
-                        <span>{location.state}</span>
+                        <span>{userName}</span>
                     </UserName>
                 </div>
-                <button type="button">
+                <button onClick={() => setIsModalActive(true)} type="button">
                     <FontAwesomeIcon icon={faPen} />
                 </button>
+                {isModalActive ?
+                    <>
+                        <EditUserDataModal
+                            userData={userName}
+                            changeUserData={setUserName}
+                            setIsModalActive={setIsModalActive} />
+                        <DarkBackground />
+                    </> : null}
             </Wrapper>
         </Container>
     )
