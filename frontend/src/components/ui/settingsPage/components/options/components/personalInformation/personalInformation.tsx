c7d3493650911789a41e9fb64994346e20981748
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2024-05-06 20:29:13 +0300

    created log out functionality

--- frontend/src/components/ui/settingsPage/components/options/components/personalInformation/personalInformation.tsx
+++ frontend/src/components/ui/settingsPage/components/options/components/personalInformation/personalInformation.tsx
@@ -1,12 +1,17 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getUserDataById } from "../../../../../../../api/authApi/authApi";
 import { UserDataType } from "../../../../../../../api/authApi/authApiTypes";
-import { getDataFromLocalStorage } from "../../../../../../../storage/localStorage/localStorage";
+import { getDataFromLocalStorage, removeDataFromLocalStorage } from "../../../../../../../storage/localStorage/localStorage";
+import { ButtonComponent } from "../../../../../../shared/button/button";
 import { Item } from "./component/item/item";
-import { List } from "./styledPersonalInformation";
+import { BtnLogOutInner, Container, List } from "./styledPersonalInformation";
+import { AuthorizedContext, AuthorizedContextType } from "../../../../../../../contexts/authorizedContext/authorizedContext";
+import { useNavigate } from "react-router-dom";
 
 export const PersonalInformation = () => {
     const [userData, setUserData] = useState<null | UserDataType>(null);
+    const authorizedContext = useContext<AuthorizedContextType>(AuthorizedContext);
+    const navigate = useNavigate()
 
     const getUserData = async () => {
         try {
@@ -18,19 +23,36 @@ export const PersonalInformation = () => {
         }
     }
 
+    const getLogOut = () => {
+        authorizedContext.logOut();
+        removeDataFromLocalStorage("token");
+        navigate("/");
+    }
+
     useEffect(() => {
         getUserData();
     }, []);
 
     return (
-        <List>
-            {userData ?
-                <>
-                    <Item link={"/settings/change-name"} category={"Name"} value={userData.name} />
-                    <Item link={"/settings/change-email"} category={"Email"} value={userData.email} />
-                    <Item link={"/settings/change-password"} category={"Password"} value={userData.password} />
-                </>
-                : null}
-        </List>
+        <Container>
+            <List>
+                {userData ?
+                    <>
+                        <Item link={"/settings/change-name"} category={"Name"} value={userData.name} />
+                        <Item link={"/settings/change-email"} category={"Email"} value={userData.email} />
+                        <Item link={"/settings/change-password"} category={"Password"} value={userData.password} />
+                    </>
+                    : null}
+            </List>
+            <BtnLogOutInner>
+                <ButtonComponent
+                    backgroundColor="#a71616"
+                    BackgroundColorHover="#820e0e"
+                    text="Log out"
+                    color="#fff"
+                    type="button"
+                    func={getLogOut} />
+            </BtnLogOutInner>
+        </Container>
     )
 }
--- frontend/src/components/ui/settingsPage/styledSettingsPage.ts
+++ frontend/src/components/ui/settingsPage/styledSettingsPage.ts
@@ -1,11 +1,5 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    max-width: 1300px;
-    margin: 0 auto;
-    padding: 0 150px;
-`
-
-export const Wrapper = styled.div`
-    padding: 90px 0;
+    padding-left: 90px;
 `
